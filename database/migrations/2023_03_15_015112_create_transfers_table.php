9edac8ebb3332f7b93047d2f3607cc9befc10b07
Kennit Ruz <kruz@merqueo.com>
2023-03-14 21:01:55 -0500

    migraciones

---
+++ database/migrations/2023_03_15_015112_create_transfers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('account_origin_id');
+            $table->unsignedInteger('account_destination_id');
+            $table->float('amount', 10);
+            $table->string('type', 10)->comment('Tipo: depósito y retiro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
